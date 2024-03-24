05491642c2abd45536b4f748e1a1114400e2d396
Code Cademy <nikolova.dev@gmail.com>
2024-03-24 19:56:49 +0000

    Added files to Task

--- README.md
+++
@@ -1,4 +0,0 @@
-Welcome to the repository for my portfolio website and projects. This repository contains the code for my personal portfolio website as well as the code for two projects I've worked on.
-
-Portfolio Website
-The portfolio website showcases my skills, projects, and experience. It is built using HTML, CSS, JavaScript, Bootsrap.
---
+++ libs/php/getTimezone.php
@@ -0,0 +1,33 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
+$lat = $_REQUEST['lat'];
+$lng = $_REQUEST['lng'];
+
+$url = 'http://api.geonames.org/timezoneJSON?lat=' . $lat . '&lng=' . $lng . '&date=2024-01-01&lang=en';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result, true);
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode;
+
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output);
