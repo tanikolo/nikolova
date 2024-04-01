c25efa92f380353ada2c847bc2f22d5a9f4e40ee
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-04-01 06:59:00 +0100

    Update getOceans.php

--- task/libs/php/getOceans.php
+++ task/libs/php/getOceans.php
@@ -26,7 +26,7 @@ $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['weatherIcao'];
+$output['data'] = $decode['ocean'];
 
 header('Content-Type: application/json; charset=UTF-8');
 
