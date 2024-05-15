264ba488316be495a0af53396a2fc23c0dcdfb8b
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-05-15 13:00:50 +0100

    Add files via upload

--- project 1/libs/js/script.js
+++ project 1/libs/js/script.js
@@ -1,161 +1,184 @@
-$(window).on('load', function () { if ($('#preloader').length) {
-  $('#preloader').delay(1000).fadeOut('slow', function () { $(this).remove();
-  }); }
-  });
+// ---------------------------------------------------------
+// GLOBAL DECLARATIONS
+// ---------------------------------------------------------
 
-const accessToken = 'G3JJegFVDpZS5IqVmwoXTx3Tfcm29hH2NPuFgYM94o3DTkqDuI00x3xyRf37dEjW';
-const map = L.map('map', {
-  center: [48.7965913, 2.3210938],
-  zoom: 3,
-  zoomControl: false  
+var map; 
+var countryLayer;
+
+// tile layers
+
+var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
+  }
+);
+
+var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+  }
+);
+
+var basemaps = {
+  "Streets": streets,
+  "Satellite": satellite
+};
+
+// buttons
+
+var infoBtn = L.easyButton("fa-info", function (btn, map) {
+  $("#modalInfo").modal("show");
 });
-const styles = ['jawg-terrain', 'jawg-streets', 'jawg-sunny', 'jawg-dark', 'jawg-light'];
-const baselayers = {};
+
+// ---------------------------------------------------------
+// EVENT HANDLERS
+// ---------------------------------------------------------
+
+// Initialise and add controls once DOM is ready
+  
+  map = L.map("map", {
+    layers: [streets]
+  }).setView([54.5, -4], 6);
+  
+
+  layerControl = L.control.layers(basemaps).addTo(map);
+
+  infoBtn.addTo(map);
 
 
-styles.forEach(style => {
-baselayers[style] = L.tileLayer(
-  `https://tile.jawg.io/${style}/{z}/{x}/{y}{r}.png?access-token=${accessToken}`, {
-    attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">&copy; <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">&copy; OSM contributors</a>',
-  });
-});
+//Adding borders to the map
 
-baselayers['jawg-terrain'].addTo(map);
-L.control.layers(baselayers, null, {
-  position: 'bottomright' 
-}).addTo(map);
+var countryLayerGroup = L.featureGroup().addTo(map);
 
-L.control.zoom({
-  position: 'bottomleft' 
-}).addTo(map);
-
-var currentGeoJsonLayer = null; 
-
-function displayCountryBorders(countryCode) {
-    if (currentGeoJsonLayer) {
-        map.removeLayer(currentGeoJsonLayer); 
+$(window).on('load', function () {
+    if ($('#preloader').length) {
+        $('#preloader').delay(1000).fadeOut('slow', function () { 
+        $(this).remove();
+        }); 
     }
+});
 
-    $.getJSON(`libs/php/countryBorders.geo.json`, function(data) {
-        const borderFeature = data.features.find(feature => feature.properties.iso_a2 === countryCode);
-        if (borderFeature) {
-            currentGeoJsonLayer = L.geoJSON(borderFeature, {
-                style: { color: "#ffc007", weight: 4, opacity: 0.65 }
-            }).addTo(map);
-            map.fitBounds(currentGeoJsonLayer.getBounds());
-        } else {
-            console.error("No borders found for country code:", countryCode);
+// Function to populate the country select dropdown
+
+function populateCountrySelect() {
+    $.ajax({
+        url: 'libs/php/getCountryData.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            if (response.status.code === 200) {
+                let countries = response.data;
+                let countrySelect = $('#countrySelect');
+                countrySelect.empty();
+                countries.forEach(function(country) {
+                    countrySelect.append(new Option(country.name, country.iso2));
+                });
+
+                countrySelect.change(function() {
+                    const selectedCountryCode = $(this).val();
+                    showCountryBorders(selectedCountryCode);
+                    showCountryInfo(selectedCountryCode);
+                });
+            } else {
+                console.error('Error loading country list:', response.status.description);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Failed to retrieve country list:', textStatus, errorThrown);
         }
-    }).fail(() => {
-        console.error("Failed to load country borders.");
     });
 }
 
-// Populate country select
-$.getJSON('libs/php/countryBorders.geo.json', function(data) {
-  const countries = data.features;
-  // Sort countries by name
-  countries.sort((a, b) => a.properties.name.localeCompare(b.properties.name));
-  
-  countries.forEach(function(feature) {
-      $('#selectCountry').append(`<option value="${feature.properties.iso_a2}">${feature.properties.name}</option>`);
-  });
-});
-
-$('#selectCountry').change(function() {
-selectedCountryCode = $(this).val();
-displayCountryBorders(selectedCountryCode);
-});
-
-var selectedCountryCode = null; 
-
-$(window).on('load', function () {
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        getCountryCode(lat, lon).then(countryCode => {
-          if (countryCode) {
-              selectedCountryCode = countryCode; 
-              displayCountryBorders(countryCode); 
-          }
-      });
-  }, function(error) {
-      console.error("Geolocation error:", error);
-      alert("Geolocation error: " + error.message);
-  });
-} else {
-  alert('Geolocation is not supported by your browser.');
+// Function to show country borders
+function showCountryBorders(countryCode) {
+    $.ajax({
+        url: 'libs/php/getCountryBorders.php',
+        type: 'GET',
+        data: { iso2: countryCode },
+        dataType: 'json',
+        success: function(response) {
+            if (response.status.code === 200) {
+                countryLayerGroup.clearLayers();
+                L.geoJSON(response.data, {
+                    style: { fillColor: "grey", weight: 2, opacity: 1, color: "black", fillOpacity: 0.3 }
+                }).addTo(countryLayerGroup);
+                map.fitBounds(countryLayerGroup.getBounds());
+            } else {
+                console.error('Error retrieving country borders:', response.status.description);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Failed to retrieve country borders:', textStatus, errorThrown);
+        }
+    });
 }
-});
 
-function getCountryCode(lat, lon) {
-  const apiKey = '65c121c9fa444dd7ae03527cd459f0be';
-  const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${apiKey}`;
-  
-  return $.getJSON(url).then(response => {
-      const countryCode = response.results[0].components.country_code.toUpperCase();
-      console.log('Country Code:', countryCode);
-      return countryCode;
-  }).catch(error => {
-      console.error('Error in reverse geocoding:', error);
-      return null;
-  });
-  }
+// Fetching current user's location using JS navigator and OpenCage API
 
-var countryInfoDiv = $('#modalInfo');
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
 
-var countryInfoModal = L.easyButton({
-  position: 'bottomleft', 
-states: [{
-  stateName: 'show-modal',
-  icon: '<i class="fas fa-info-circle text-dark"></i>',
-  title: 'Country Information',
-  onClick: function() {
-    var countryCode = $('#countryCodeInput').val();
-      showCountryInfo(countryCode);
-      console.log("Country code being sent: ", countryCode);
+        $.ajax({
+            url: 'libs/php/getCountryCode.php',
+            type: 'GET',
+            data: {
+                lat: lat,
+                lon: lon
+            },
+            dataType: 'json',
+            success: function(response) {
+                if (response.status.code === 200) {
+                    let countryCode = response.data.countryCode;
+                    console.log('Detected country code:', countryCode);
+                    $('#countrySelect').val(countryCode);
+                    showCountryBorders(countryCode);
+                } else {
+                    console.error('Error retrieving country code:', response.status.description);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('Failed to retrieve country code:', textStatus, errorThrown);
+            }
+        });
+    }, function(error) {
+        console.error('Geolocation error:', error);
+    });
+} else {
+    console.error('Geolocation is not supported by this browser.');
+}
 
-      countryInfoDiv.modal('show');
-  }
-}]
-}).addTo(map);
+populateCountrySelect();
 
-countryInfoModal.button.style.backgroundColor = '#FFC007';  
+// Function to show country info in a modal
 
 function showCountryInfo(countryCode) {
-$.ajax({
-    url: "libs/php/getCountryInfo.php",
-    type: "GET", 
-    dataType: 'json',
-    data: {
-        geonamesInfo: countryCode 
-    },
-    success: function(result) {
-      if (result.status === "ok") {
-        updateCountryInfo(result.data);
-    } else {
-        console.error('Server error: ' + result.error);
-        displayError('Failed to fetch country information: ' + result.error);
-    }
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.error('AJAX error: ' + textStatus + ' - ' + errorThrown);
-        displayError('Error in AJAX request: ' + textStatus);
-    }
-});
-}
+    console.log(countryCode);
+    $.ajax({
+        url: 'libs/php/getCountryInfo.php',
+        type: 'GET',
+        dataType: 'json',
+        data: { country: countryCode },
+        success: function(result) {
+            console.log(JSON.stringify(result));
 
-function updateCountryInfo(countryInfo) {
-$('#continent').html(countryInfo.continent);
-$('#countryName').html(countryInfo.countryName);
-$('#capital').html(countryInfo.capital);
-$('#population').html(numeral(countryInfo.population).format('0,0'));
-$('#languages').html(countryInfo.languages.join(', '));
-$('#areaInSqKm').html(numeral(countryInfo.areaInSqKm).format('0,0') + ' km²');
-$('#currency').html(countryInfo.currency);
-}
+            if (result.status.code === 200) {
+                const country = result.data[0];
 
-function displayError(message) {
-$('#errorContainer').html(message).show();
+                $('#country').text(country.countryName);
+                $('#capital').text(country.capital);
+                $('#continent').text(country.continentName);
+                $('#population').text(country.population);
+                $('#languages').text(country.languages);
+                $('#currency').text(country.currencyCode);
+                $('#area').text(country.areaInSqKm + " sq km");
+
+                $('#modalInfo').modal('show');
+            } else {
+                console.error("Error loading country data:", result.status.description);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('AJAX error:', textStatus, errorThrown);
+        }
+    });
 }
