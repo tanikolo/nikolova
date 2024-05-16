2bced4025b2a0855b584f9a703290f80be21dfee
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-05-16 08:21:04 +0100

    Add files via upload

--- project 1/libs/js/script.js
+++ project 1/libs/js/script.js
@@ -44,141 +44,33 @@ var infoBtn = L.easyButton("fa-info", function (btn, map) {
   infoBtn.addTo(map);
 
 
-//Adding borders to the map
 
-var countryLayerGroup = L.featureGroup().addTo(map);
-
-$(window).on('load', function () {
-    if ($('#preloader').length) {
-        $('#preloader').delay(1000).fadeOut('slow', function () { 
-        $(this).remove();
-        }); 
-    }
-});
 
 // Function to populate the country select dropdown
 
 function populateCountrySelect() {
     $.ajax({
-        url: 'libs/php/getCountryData.php',
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            if (response.status.code === 200) {
-                let countries = response.data;
-                let countrySelect = $('#countrySelect');
-                countrySelect.empty();
-                countries.forEach(function(country) {
-                    countrySelect.append(new Option(country.name, country.iso2));
-                });
-
-                countrySelect.change(function() {
-                    const selectedCountryCode = $(this).val();
-                    showCountryBorders(selectedCountryCode);
-                    showCountryInfo(selectedCountryCode);
-                });
+        url: "libs/php/getCountries.php",
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            console.log('AJAX call successful. Result:', result); 
+            if (result.status.code !== 200) {
+                console.log('Error reading geoJson file!');
             } else {
-                console.error('Error loading country list:', response.status.description);
+                result.data.forEach(data => {
+                    console.log('Adding country:', data); 
+                    $('#countrySelect').append("<option value=\"" + data.code + "\">" + data.name + "</option>");
+                });
             }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('Failed to retrieve country list:', textStatus, errorThrown);
+        error: function(xhr, status, error) {
+            console.log('Error');
+            console.log(error);
         }
     });
 }
 
-// Function to show country borders
-function showCountryBorders(countryCode) {
-    $.ajax({
-        url: 'libs/php/getCountryBorders.php',
-        type: 'GET',
-        data: { iso2: countryCode },
-        dataType: 'json',
-        success: function(response) {
-            if (response.status.code === 200) {
-                countryLayerGroup.clearLayers();
-                L.geoJSON(response.data, {
-                    style: { fillColor: "grey", weight: 2, opacity: 1, color: "black", fillOpacity: 0.3 }
-                }).addTo(countryLayerGroup);
-                map.fitBounds(countryLayerGroup.getBounds());
-            } else {
-                console.error('Error retrieving country borders:', response.status.description);
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('Failed to retrieve country borders:', textStatus, errorThrown);
-        }
-    });
-}
-
-// Fetching current user's location using JS navigator and OpenCage API
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-
-        $.ajax({
-            url: 'libs/php/getCountryCode.php',
-            type: 'GET',
-            data: {
-                lat: lat,
-                lon: lon
-            },
-            dataType: 'json',
-            success: function(response) {
-                if (response.status.code === 200) {
-                    let countryCode = response.data.countryCode;
-                    console.log('Detected country code:', countryCode);
-                    $('#countrySelect').val(countryCode);
-                    showCountryBorders(countryCode);
-                } else {
-                    console.error('Error retrieving country code:', response.status.description);
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error('Failed to retrieve country code:', textStatus, errorThrown);
-            }
-        });
-    }, function(error) {
-        console.error('Geolocation error:', error);
-    });
-} else {
-    console.error('Geolocation is not supported by this browser.');
-}
 
 populateCountrySelect();
 
-// Function to show country info in a modal
-
-function showCountryInfo(countryCode) {
-    console.log(countryCode);
-    $.ajax({
-        url: 'libs/php/getCountryInfo.php',
-        type: 'GET',
-        dataType: 'json',
-        data: { country: countryCode },
-        success: function(result) {
-            console.log(JSON.stringify(result));
-
-            if (result.status.code === 200) {
-                const country = result.data[0];
-
-                $('#country').text(country.countryName);
-                $('#capital').text(country.capital);
-                $('#continent').text(country.continentName);
-                $('#population').text(country.population);
-                $('#languages').text(country.languages);
-                $('#currency').text(country.currencyCode);
-                $('#area').text(country.areaInSqKm + " sq km");
-
-                $('#modalInfo').modal('show');
-            } else {
-                console.error("Error loading country data:", result.status.description);
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('AJAX error:', textStatus, errorThrown);
-        }
-    });
-}
