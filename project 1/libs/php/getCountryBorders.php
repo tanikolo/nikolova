8af240246dd4623974e753e292a21e88a29f7046
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-05-29 23:46:36 +0100

    Remove comments

--- project 1/libs/php/getCountryBorders.php
+++ project 1/libs/php/getCountryBorders.php
@@ -1,13 +1,10 @@
 <?php
 
-// Display errors for debugging
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-// Read the country borders geojson file
 $result = file_get_contents("countryBorders.geo.json");
 
-// Return the content as JSON
 header('Content-Type: application/json');
 echo $result;
 ?>
