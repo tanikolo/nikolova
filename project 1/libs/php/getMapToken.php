1226511bf0d129ba1e0adcc734d59490f361a89f
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-06-09 01:11:24 +0100

    Add extensive error checking

--- project 1/libs/php/getMapToken.php
+++ project 1/libs/php/getMapToken.php
@@ -1,4 +1,39 @@
 <?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    echo json_encode([
+        'status' => [
+            'code' => 405,
+            'name' => 'error',
+            'description' => 'Invalid request method'
+        ]
+    ]);
+    exit;
+}
+
 $accessToken = 'G3JJegFVDpZS5IqVmwoXTx3Tfcm29hH2NPuFgYM94o3DTkqDuI00x3xyRf37dEjW';
-echo json_encode(['token' => $accessToken]);
+
+if (empty($accessToken)) {
+    echo json_encode([
+        'status' => [
+            'code' => 500,
+            'name' => 'error',
+            'description' => 'Access token is missing'
+        ]
+    ]);
+    exit;
+}
+
+echo json_encode([
+    'status' => [
+        'code' => 200,
+        'name' => 'ok',
+        'description' => 'success'
+    ],
+    'token' => $accessToken
+]);
+
 ?>
