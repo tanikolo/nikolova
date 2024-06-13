65162f4fbeb7a53f0e1f5e0b2e3de9a6c4b8174f
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-06-13 18:24:48 +0100

    Add function populatePersonnelTable

--- project2/libs/js/script.js
+++ project2/libs/js/script.js
@@ -1,1 +1,46 @@
+$(document).ready(function() {
 
+    function populatePersonnelTable() {
+      $.ajax({
+        url: 'libs/php/getAll.php', 
+        method: 'GET',
+        dataType: 'json',
+        success: function(response) {
+          console.log('Response from getAll.php:', response); 
+          if (response.status.code === "200" && response.data) {
+            const personnel = response.data;
+            const personnelTableBody = $("#personnelTableBody");
+            personnelTableBody.empty();
+  
+            personnel.forEach(person => {
+              const row = `
+                <tr>
+                  <td class="align-middle text-nowrap">${person.firstName}, ${person.lastName}</td>
+                  <td class="align-middle text-nowrap d-none d-md-table-cell">${person.department}</td>
+                  <td class="align-middle text-nowrap d-none d-md-table-cell">${person.location}</td>
+                  <td class="align-middle text-nowrap d-none d-md-table-cell">${person.email}</td>
+                  <td class="text-end text-nowrap">
+                    <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editPersonnelModal" data-id="${person.id}">
+                      <i class="fa-solid fa-pencil fa-fw"></i>
+                    </button>
+                    <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#deletePersonnelModal" data-id="${person.id}">
+                      <i class="fa-solid fa-trash fa-fw"></i>
+                    </button>
+                  </td>
+                </tr>
+              `;
+              personnelTableBody.append(row);
+            });
+          } else {
+            console.error('Failed to fetch personnel data:', response.status.description);
+          }
+        },
+        error: function(error) {
+          console.error('Error fetching personnel data:', error);
+        }
+      });
+    }
+  
+    populatePersonnelTable();
+  
+  });
