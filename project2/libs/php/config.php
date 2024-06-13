0be3e818497f926071a7565eb09fd08cea19c2c5
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-06-13 18:31:04 +0100

    Update config.php

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,16 +1,12 @@
-
 <?php
+$host_name = 'db5015939957.hosting-data.io';
+$database = 'dbs12991741';
+$user_name = 'dbu2677101';
+$password = 'R@inb0wd@y2024';
 
-	// connection details for MySQL database
+$link = new mysqli($host_name, $user_name, $password, $database);
 
-	$cd_host = "127.0.0.1";
-	$cd_port = 3306;
-	$cd_socket = "";
-
-	// database name, username and password
-
-	$cd_dbname = "companydirectory";
-	$cd_user = "companydirectory";
-	$cd_password = "companydirectory";
-
+if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+}
 ?>
