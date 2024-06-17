d9b290b7ef1dc7e9421ad4ddf32480f4e5359a85
Tatiana Nikolova <108439898+tanikolo@users.noreply.github.com>
2024-06-17 23:10:57 +0100

    Update SearchLocations.php

--- project2/libs/php/SearchLocations.php
+++ project2/libs/php/SearchLocations.php
@@ -1,1 +1,78 @@
+<?php
 
+$executionStartTime = microtime(true);
+
+include(__DIR__ . "/config.php");
+
+header('Content-Type: application/json; charset=UTF-8');
+
+$conn = new mysqli($host_name, $user_name, $password, $database);
+
+if (mysqli_connect_errno()) {
+    $output['status']['code'] = "300";
+    $output['status']['name'] = "failure";
+    $output['status']['description'] = "database unavailable";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+    echo json_encode($output);
+    exit;
+}
+
+$searchText = isset($_POST['txt']) ? '%' . $conn->real_escape_string($_POST['txt']) . '%' : '%';
+
+$query = $conn->prepare('SELECT id, name FROM location WHERE name LIKE ? ORDER BY name');
+
+if (false === $query) {
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "failed";
+    $output['status']['description'] = "Query preparation failed: " . $conn->error;
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+    echo json_encode($output);
+    $conn->close();
+    exit;
+}
+
+$query->bind_param('s', $searchText);
+
+$query->execute();
+
+if (false === $query) {
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "failed";
+    $output['status']['description'] = "Query execution failed: " . $query->error;
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+    echo json_encode($output);
+    $conn->close();
+    exit;
+}
+
+$result = $query->get_result();
+if (false === $result) {
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "failed";
+    $output['status']['description'] = "Result fetching failed: " . $query->error;
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+    echo json_encode($output);
+    $conn->close();
+    exit;
+}
+
+$data = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    array_push($data, $row);
+}
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+$output['data'] = $data;
+
+echo json_encode($output);
+
+$conn->close();
+?>
